bd5a3dab896287171d592d13bda5f310808ae8dd
Zora <zora.wangyanzi@gmail.com>
2022-10-23 16:01:20 +0000

    feat: update theme colors (pull request #21)

--- src/theme/index.js
+++ src/theme/index.js
@@ -15,6 +15,61 @@ const theme = createTheme({
       background_light_gray: '#E3E3E3',
     },
   },
+  colors: {
+    /* List of all the colors, non-semantic */
+    background_lightest_grey: '#F8F8F8',
+    background_lighter_grey: '#F1F1F1',
+    background_light_grey: '#E3E3E3',
+    background_grey: '#7E7F80',
+    button_purple: '#696784',
+    background_light_purple: '#5E5962',
+    background_purple: '#464249',
+    text_light_grey: '#EDEDED',
+    text_highlight_grey: '#DFDFDF',
+    text_blue: '#227ACA',
+
+    /* The following would be the colors with semantic name corresponding to each component */
+    /* Sign in & Register Page colors */
+    globalBG_grey: '#F8F8F8', // this globalBG could be used as the big area BG color in every page
+    signInBG_grey: '#F8F8F8',
+    inputBG_grey: '#E3E3E3',
+    submitButton_purple: '#696784',
+    registerForgotPWD_blue: '#227ACA',
+
+    /* Home Page Swiper section colors */
+    homePageBG_grey: '#7E7F80',
+    searchBG_purple: '#464249',
+    swiperBG_purple: '#464249',
+    swiperCardBG_purple: '#5E5962',
+
+    /* Home Page Review section colors */
+    reviewBG_grey: '#E3E3E3',
+    sortByBG_grey: '#E3E3E3',
+    sortBy_highlight_grey: '#7E7F80',
+    trendingTagBG_grey: '#E3E3E3',
+    trendingTagBG_iPhone_grey: '#F1F1F1',
+    tagCardBG_grey: '#CECECE',
+    category_highlight_grey: '#E3E3E3',
+    highlighted_text_grey: '#DFDFDF',
+
+    /* Post Page colors */
+    columnsBG_grey: '#F1F1F1',
+    otherReviewsBG_grey: '#E3E3E3',
+    commentsBG_grey: '#F1F1F1',
+
+    /* User Page colors */
+    authorInfoBG_grey: '#F1F1F1',
+    userTagBG_grey: '#F1F1F1',
+    userTagCardBG_grey: '#E3E3E3',
+    clicksBG_grey: '#E3E3E3',
+    userReviewBG_grey: '#F1F1F1',
+
+    /* Movie Page colors (sortBy & button colors are in global) */
+    introductionBG_grey: '#F1F1F1',
+    movieTagBG_grey: '#F1F1F1',
+    movieTagCardBG_grey: '#E3E3E3',
+    movieReviewBG_grey: '#F1F1F1',
+  },
   breakpoints: {
     miniMobile: '0px',
     mobile: '320px',
