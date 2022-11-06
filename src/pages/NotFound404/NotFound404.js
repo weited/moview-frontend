e03b64c04343c76458173df0aa18fb9ee66a541a
Jeffrey Lin <jeffreylin0312@gmail.com>
2022-11-06 14:37:05 +0000

    feat: update 404 not found page (pull request #41)
    * feat: add a button to navigate to home page

--- src/pages/NotFound404/NotFound404.js
+++ src/pages/NotFound404/NotFound404.js
@@ -1,10 +1,38 @@
-import { useRouteError } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import { Button as MuiButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import pagenotfoundImage from '../../assets/img/pagenotfound.jpg';
+
+const Section = styled('section')({
+  textAlign: 'center',
+});
+
+const Button = styled(MuiButton)(() => ({
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
 
 function NotFound404() {
-  const error = useRouteError();
-  // eslint-disable-next-line no-console
-  console.error(error);
-  return <div>This is NotFound404</div>;
+  const navigate = useNavigate();
+  return (
+    <Section>
+      <h1>Oops..! 404 Page Not Found</h1>
+      <Button variant="contained" onClick={() => navigate('/')}>
+        Back to Home
+      </Button>
+      <p>Looks like you came to wrong page on our server</p>
+      <Box
+        component="img"
+        sx={{
+          height: 500,
+          width: 500,
+        }}
+        alt="404 not found image."
+        src={pagenotfoundImage}
+      />
+    </Section>
+  );
 }
 
 export default NotFound404;
