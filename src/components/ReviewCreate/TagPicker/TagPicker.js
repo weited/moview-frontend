fb36ceec7a00fcb6167b280ea966ad33c9cac149
Weite Dai <weitedai.dev@gmail.com>
2022-11-16 15:17:46 +1100

    chore(eslint): turn off props spread warning

--- src/components/ReviewCreate/TagPicker/TagPicker.js
+++ src/components/ReviewCreate/TagPicker/TagPicker.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
@@ -7,19 +7,12 @@ import { selectTags } from '../../../redux/slices/tag';
 const filter = createFilterOptions();
 
 export default function TagPicker({ value, setValue }) {
-  // const [value, setValue] = useState([]);
-
-  // const [isMax, setIsMax] = useState(false);
-
   const tagList = useSelector(selectTags);
-  console.log('value tags', value);
 
   useEffect(() => {
-    console.log('value length', value.length);
     if (value.length > 4) {
       // setIsMax(true);
       setValue(value.slice(0, -1));
-      console.log('maxm');
     }
   }, [value]);
 
@@ -28,16 +21,12 @@ export default function TagPicker({ value, setValue }) {
       multiple
       value={value}
       onChange={(event, newValue) => {
-        console.log('change', newValue);
         if (typeof newValue === 'string') {
-          console.log('string', newValue);
           setValue({
             name: newValue,
           });
         } else if (newValue && newValue.length > 0 && newValue[newValue.length - 1].inputValue) {
           // Create a new value from the user input
-          console.log('newValue && new.input', newValue);
-
           setValue([
             ...value,
             {
@@ -45,13 +34,10 @@ export default function TagPicker({ value, setValue }) {
             },
           ]);
         } else {
-          console.log('else', newValue);
-
           setValue(newValue);
         }
       }}
       filterOptions={(options, params) => {
-        // console.log('filterOptions', params);
         const filtered = filter(options, params);
 
         const { inputValue } = params;
@@ -72,7 +58,6 @@ export default function TagPicker({ value, setValue }) {
       id="tag-picker"
       options={tagList}
       getOptionLabel={(option) => {
-        // console.log('getOptionLabel', option);
         // Value selected with enter, right from the input
         if (typeof option === 'string') {
           return option;
@@ -91,14 +76,3 @@ export default function TagPicker({ value, setValue }) {
     />
   );
 }
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-// const top100Films = [
-//   { name: 'The Shawshank Redemption', year: 1994 },
-//   { name: 'The Godfather', year: 1972 },
-//   { name: 'The Godfather: Part II', year: 1974 },
-//   { name: 'The Dark Knight', year: 2008 },
-//   { name: '12 Angry Men', year: 1957 },
-//   { name: "Schindler's List", year: 1993 },
-//   { name: 'Pulp Fiction', year: 1994 },
-// ];
