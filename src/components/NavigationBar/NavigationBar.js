100952665d9803826f9e1331d74bc8255a258f0e
Weite Dai <weitedai.dev@gmail.com>
2022-11-21 15:12:48 +1100

    feat(header): add profile menu and log out

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -3,8 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import { logout } from '../../redux/slices/user';
@@ -35,8 +41,7 @@ const UserInfo = styled.span(
   cursor: pointer;
   @media (min-width:${theme.breakpoints.largeLaptop}) {
     font-size: 24px;
-}
-  `
+}`
 );
 
 export default function NavigationBar() {
@@ -49,12 +54,28 @@ export default function NavigationBar() {
 
   const isHomePage = path.pathname === '/';
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    if (!isLogin) {
+      return;
+    }
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleAuth = () => {
     if (!isLogin) {
       navigate('/login');
     } else {
       dispatch(logout());
     }
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
   };
 
   return (
@@ -68,8 +89,66 @@ export default function NavigationBar() {
       >
         <TextField fullWidth id="outlined-basic" label="" variant="outlined" size="small" />
       </Box>
-      <UserInfo onClick={handleAuth}>{isLogin ? 'Logout' : 'Sign in'}</UserInfo>
-      <AccountCircleIcon sx={{ fontSize: { sm: 50, lg: 64 }, color: '#D0D0D0' }} />
+      {!isLogin && <UserInfo onClick={handleAuth}>Sign in</UserInfo>}
+      {isLogin && (
+        <AccountCircleIcon
+          onClick={handleClick}
+          sx={{ fontSize: { sm: 50, lg: 64 }, color: '#D0D0D0', cursor: 'pointer' }}
+        />
+      )}
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            minWidth: '200px',
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleLogout}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
     </Container>
   );
 }
